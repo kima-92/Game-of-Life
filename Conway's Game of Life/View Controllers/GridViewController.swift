0f36a902eb5b3a51eebab4c9f5c7000865df9a98
Wilma <kima_92@fastmail.com>
2020-05-26 11:36:11 -0400

    Wired the GridViewController from the UI to it's class file

---
+++ Conway's Game of Life/View Controllers/GridViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GridViewController.swift
+//  Conway's Game of Life
+//
+//  Created by macbook on 5/25/20.
+//  Copyright © 2020 WilmaRodriguez. All rights reserved.
+//
+
+import UIKit
+
+class GridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    @IBAction func gridButtonTapped(_ sender: UIButton) {
+
+    }
+    
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
